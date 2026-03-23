f926dd081ab0047ce576d4e5848dd93799491e8b
vouchneayun0-create <vouchneayun0@gmail.com>
2026-03-23 18:32:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,182 @@
+const iphoneData = [
+  {
+    series: "iPhone 17 Series",
+    models: [
+      {
+        name: "iPhone 17 Pro Max",
+        specs: "6.9\" Display, A19 Pro,Titanium",
+        price: "$1,199",
+        image: "image/iph.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Pro Fusion Camera System",
+        battery: "Fast-charge capable",
+        colors: [
+          { name: "Titanium", hex: "#bebebe", image: "image/iph.png" },
+          { name: "Cosmic Orange", hex: "#ff8c00", image: "image/17maxo.png" },
+          { name: "Blue", hex: " #0000ff", image: "image/17maxb.png" },
+        ]
+      },
+      {
+        name: "iPhone 17 Pro",
+        specs: "6.3\" Display, A19 Pro Chip, Cosmic Orange",
+        price: "$1,099",
+        image: "image/17pro.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Pro Fusion Camera System",
+        battery: "Fast-charge capable",
+        colors: [
+          { name: "Titanium", hex: "#bebebe", image: "image/17prot.png" },
+          { name: "Cosmic Orange", hex: "#ff8c00", image: "image/17pro.png" },
+          { name: "Blue", hex: " #0000ff", image: "image/17prob.png" },
+          { hex: "white", image: "image/17prom.png" },
+        ]
+      },
+      {
+        name: "iPhone 17 Air",
+        specs: "6mm Ultra-thin, 6.6\" Display",
+        price: "$999",
+        image: "image/17air.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Fusion Camera System",
+        battery: "Fast-charge capable",
+        colors: [
+          { name: "Titanium", hex: "#bebebe", image: "image/17airt.png" },
+          { name: "Gold", hex: "#f8dbb8ff", image: "image/17airg.png" },
+          { name: "Black", hex: "#000", image: "image/17airb.png" },
+          { name: "Sky Blue", hex: " #b5deefff", image: "image/17air.png" },
+        ]
+      },
+      {
+        name: "iPhone 17",
+        specs: "6.3\" Display, A19 Chip",
+        price: "$799",
+        image: "image/17.png",
+        storage: "256GB / 512GB",
+        camera: "48MP Dual Fusion Camera System",
+        battery: "Fast-charge capable",
+        colors: [
+          { name: "Titanium", hex: "#bebebe", image: "image/17.png" },
+          { name: "Green", hex: "#8ae9c0ff", image: "image/17green.png" },
+          { name: "Purple", hex: "#e5c0f1ff", image: "image/17p.png" },
+          { name: "Sky Blue", hex: "#b5deefff ", image: "image/17b.png" },
+        ]
+      }
+    ]
+  },
+  {
+    series: "iPhone 16 Series",
+    models: [
+      {
+        name: "iPhone 16 Pro Max",
+        specs: "6.9\" Display, A18 Pro",
+        price: "$1,099",
+        image: "image/16promax.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Triple Camera",
+        battery: "All-day battery life",
+        colors: [
+          { name: "White Titanium", hex: "#bebebe", image: "image/16promax_t.png" },
+          { name: "Black Titanium", hex: "#201f1dad", image: "image/16promax_black.png" },
+          { name: "Natural", hex: " #fcd8a5ff", image: "image/16promax_natural.png" },
+          { name: "Gold", hex: " #faad40f6", image: "image/16promaxgold.png" },
+          { name: "All color", hex: "#bebebe", image: "image/16promaxall.png" }
+        ]
+      },
+      {
+        name: "iPhone 16 Plus",
+        specs: "6.1\" Display, A18 Chip",
+        price: "$899",
+        image: "image/16plusp.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Triple Camera",
+        battery: "All-day battery life",
+        colors: [
+          { name: "White", hex: "#bebebe79", image: "image/16plusp.png" },
+          { name: "Teal", hex: "#6ea28cff", image: "image/16teal.png" },
+          { name: "Pink", hex: "#f1c0d8ff", image: "image/16pluspinkie.png" },
+          { name: "All Colors", hex: "#cbe8f53f ", image: "image/16a.png" },
+        ]
+      },
+      {
+        name: "iPhone 16",
+        specs: "6.1\" Display, A18 Chip",
+        price: "$699",
+        image: "image/16b.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Triple Camera",
+        battery: "All-day battery life",
+        colors: [
+          { name: "White", hex: "#bebebe79", image: "image/16plusp.png" },
+          { name: "Teal", hex: "#6ea28cff", image: "image/16teal.png" },
+          { name: "Pink", hex: "#f1c0d8ff", image: "image/16pink.png" },
+          { name: "Black", hex: "#000", image: "image/16b.png" },
+          { name: "All Colors", hex: "#cbe8f53f ", image: "image/16mix.png" },
+        ]
+      },
+      {
+        name: "iPhone 16e",
+        specs: "Budget Entry, A18 Chip",
+        price: "$599",
+        image: "image/16e.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Triple Camera",
+        battery: "All-day battery life",
+        colors: [
+          { name: "Black", hex: "#000", image: "image/16eblack.png" },
+          { name: "White", hex: "#bebebe", image: "image/16e.png" },
+          { name: "White Detail", hex: "#bebebe", image: "image/16ewhite.png" },
+        ]
+      }
+    ]
+  },
+  {
+    series: " iPhone 15 series",
+    models: [
+      {
+        name: "iPhone 15 Pro Max",
+        specs: "6.9\" Display, A18 Pro",
+        price: "$899",
+        image: "image/15promaxblack.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Triple Camera",
+        battery: "All-day battery life",
+        colors: [
+          { name: "White Titanium", hex: "#bebebe", image: "image/15promaxwhite.png" },
+          { name: "Black Titanium", hex: "#201f1dad", image: "image/15promaxblack.png" },
+          { name: "Blue Titanium", hex: " #3e6089f6", image: "image/15promaxblue.png" },
+        ]
+      },
+      {
+        name: "iPhone Pro",
+        specs: "6.1\" Display, A18 Chip",
+        price: "$799",
+        image: "image/15pronatural.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Triple Camera",
+        battery: "All-day battery life",
+        colors: [
+          { name: "White Titanium", hex: "#bebebe", image: "image/15prowhite.png" },
+          { name: "Black Titanium", hex: "#201f1dad", image: "image/15problack.png" },
+          { name: "Blue Titanium", hex: " #3e6089f6", image: "image/15problue.png" },
+          { name: "Natural", hex: " #efe5bdf6", image: "image/15pronatural.png" },
+        ]
+      },
+      {
+        name: "iPhone 15",
+        specs: "6.1\" Display, A18 Chip",
+        price: "$599",
+        image: "image/15skyblue.png",
+        storage: "256GB / 512GB / 1TB",
+        camera: "48MP Triple Camera",
+        battery: "All-day battery life",
+        colors: [
+          { name: "Black", hex: "#010000c2", image: "image/15black.png" },
+          { name: "Green", hex: "#97e8a0ff", image: "image/15green.png" },
+          { name: "Pink", hex: "#f1c0d8ff", image: "image/15pink.png" },
+          { name: "Sky Blue", hex: " rgba(195, 226, 241, 1)", image: "image/15skyblue.png" },
+          { name: "Yellow", hex: "#f6f2b9ff", image: "image/15yellow.png" }
+        ]
+      },
+    ]
+  }
+];
